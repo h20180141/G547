30f85df6e19a6dd6ff163d42babeb139a3835b4e
h20180141 <47377675+h20180141@users.noreply.github.com>
2019-02-06 12:20:06 +0530

    Add files via upload

--- assignment_1/chardriver_kernel.c
+++ assignment_1/chardriver_kernel.c
@@ -15,8 +15,6 @@
 static dev_t aclr;   // variable for device number
 
 static struct cdev *x_dev;
-static struct cdev *y_dev;
-static struct cdev *z_dev;
 
 static struct class *cls;
 
@@ -45,55 +43,19 @@ uint16_t getrand(void)
 
 // STEP 4 : Driver Call back functions 
 
-static int x_open(struct inode *i,struct file *f)
+static int f_open(struct inode *i,struct file *f)
 {
-	printk(KERN_INFO "x:open()\n");
+	printk(KERN_INFO "f:open()\n");
 	return 0;
 }
-static int x_close(struct inode *i,struct file *f)
+static int f_close(struct inode *i,struct file *f)
 {
-	printk(KERN_INFO "x:close()\n");
+	printk(KERN_INFO "f:close()\n");
 	return 0;
 }
-static ssize_t x_read(struct file *f,char __user *buf,size_t len,loff_t *off)
+static ssize_t f_read(struct file *f,char __user *buf,size_t len,loff_t *off)
 {
-	printk(KERN_INFO "x:read()\n");
-	val=getrand()&0x3ff;
-	printk(KERN_INFO "%d\n",val);
-	copy_to_user(buf, &val, 2);
-	return 0;
-}
-static int y_open(struct inode *i,struct file *f)
-{
-	printk(KERN_INFO "y:open()\n");
-	return 0;
-}
-static int y_close(struct inode *i,struct file *f)
-{
-	printk(KERN_INFO "y:close()\n");
-	return 0;
-}
-static ssize_t y_read(struct file *f,char __user *buf,size_t len,loff_t *off)
-{
-	printk(KERN_INFO "y:read()\n");
-	val=getrand()&0x3ff;
-	printk(KERN_INFO "%d\n",val);
-	copy_to_user(buf, &val, 2);
-	return 0;
-}
-static int z_open(struct inode *i,struct file *f)
-{
-	printk(KERN_INFO "z:open()\n");
-	return 0;
-}
-static int z_close(struct inode *i,struct file *f)
-{
-	printk(KERN_INFO "z:close()\n");
-	return 0;
-}
-static ssize_t z_read(struct file *f,char __user *buf,size_t len,loff_t *off)
-{
-	printk(KERN_INFO "z:read()\n");
+	printk(KERN_INFO "f:read()\n");
 	val=getrand()&0x3ff;
 	printk(KERN_INFO "%d\n",val);
 	copy_to_user(buf, &val, 2);
@@ -102,28 +64,13 @@ static ssize_t z_read(struct file *f,char __user *buf,size_t len,loff_t *off)
 
 // File operations to be performed on files 
 
-static struct file_operations x_ops =
+static struct file_operations fops =
 {
 	.owner=THIS_MODULE,
-	.open=x_open,
-	.release=x_close,
-	.read=x_read,
+	.open=f_open,
+	.release=f_close,
+	.read=f_read,
 };
-static struct file_operations y_ops =
-{
-	.owner=THIS_MODULE,
-	.open=y_open,
-	.release=y_close,
-	.read=y_read,
-};
-static struct file_operations z_ops =
-{
-	.owner=THIS_MODULE,
-	.open=z_open,
-	.release=z_close,
-	.read=z_read,
-};
-
 
 static __init int demo_init(void)
 {
@@ -156,58 +103,31 @@ static __init int demo_init(void)
 	// STEP 3 : Link fops and cdev to the device node
 
 		
-	x_dev = cdev_alloc(); // struct cdev is allocated dynamically and its pointer address stored in x_dev
+	 // struct cdev is allocated dynamically and its pointer address stored in x_dev
 					
-	cdev_init(x_dev,&x_ops); // initialises the struct cdev with the defined file operations 
-
+	
 	device_create(cls,NULL,x,NULL,"x_axis"); // file 'x_axis' is ceated in /dev directory
-
-	if(cdev_add(x_dev,x,1)<0)  // cdev_add() : it adds the charatcer device to the system, after successfull addition it returns 0 
-		{
-			class_destroy(cls);
-			unregister_chrdev_region(aclr, 3);
-			return -1;
-		}
-
-	
-	y_dev = cdev_alloc();
-	cdev_init(y_dev,&y_ops);
 	device_create(cls,NULL,y,NULL,"y_axis");
-
-	if(cdev_add(y_dev,y,1)<0)
-		{
-			cdev_del(x_dev);
-			device_destroy(cls, y);
-			device_destroy(cls, x);
-			class_destroy(cls);
-			unregister_chrdev_region(aclr, 3);
-			return -1;
-		}
-
-	
-	z_dev = cdev_alloc();
-	cdev_init(z_dev,&z_ops);
 	device_create(cls,NULL,z,NULL,"z_axis");
 
-	if(cdev_add(z_dev,z,1)<0)
+	x_dev = cdev_alloc();
+	cdev_init(x_dev,&fops); // initialises the struct cdev with the defined file operations 
+
+
+
+	if(cdev_add(x_dev,aclr,3)<0)  // cdev_add() : it adds the charatcer device to the system, after successfull addition it returns 0 
 		{
-			cdev_del(y_dev);
-			cdev_del(x_dev);
-			device_destroy(cls, z);
-			device_destroy(cls, y);
-			device_destroy(cls, x);
 			class_destroy(cls);
 			unregister_chrdev_region(aclr, 3);
 			return -1;
 		}
+
 	
+
 }
 static __exit void demo_exit(void)
 {
-	//printk(KERN_ALERT "BYE WORLD\n");
 	cdev_del(x_dev);
-	cdev_del(y_dev);
-	cdev_del(z_dev);
 	device_destroy(cls,x);
 	device_destroy(cls,y);
 	device_destroy(cls,z);
